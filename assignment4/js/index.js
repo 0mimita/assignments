dd13fa449e8d35a26ec9ffe91f27adec1563ac9c
Mariam Tumah <tumahmariam@gmail.com>
2026-03-29 22:36:52 +0200

    filtrering

--- assignment4/js/index.js
+++ assignment4/js/index.js
@@ -1,4 +1,4 @@
-import { getScareLevelText } from "./utils";
+import { getScareLevelText } from "./utils.js";
 
 let allHouses = [];
 
@@ -20,7 +20,59 @@ async function fetchHouses() {
 
     } catch (error) {
         console.error("Fel:", error);
-        houseGrid.innerHTML = '<p>Kunde inte ladda husen: ${error.message}</p>'
+        houseGrid.innerHTML = `<p>Kunde inte ladda husen: ${error.message}</p>`
     }
 }
 
+function renderHouses(housesToDisplay) {
+    houseGrid.innerHTML = "";
+
+    if (housesToDisplay.length === 0) {
+        houseGrid.innerHTML = "<p class='no-results'>Inga spökhus matchar din sökning...</p>";
+        return;
+    }
+
+    //klicka på husen
+    housesToDisplay.forEach(house => {
+        const card = document.createElement("a");
+        card.href = `house.html?id=${house.id}`;
+        card.className = "house-card";
+
+        card.innerHTML = `
+            <img src="images/${house.image}" alt="${house.name}">
+            <div class="house-info">
+                <h3>${house.name}</h3>
+                <p class="location">${house.location.toUpperCase()}</p>
+                <div class="price-row">
+                    <span>${house.pricePerNight} KR/NATT</span>
+                    <span class="scare-tag">${getScareLevelText(house.scareLevel)}</span>
+                </div>
+            </div>
+        `;
+
+        houseGrid.appendChild(card);
+    });
+}
+
+filterForm.addEventListener("input", () => {
+    const maxPrice = document.getElementById("max-price").value || Infinity;
+    const minScare = document.getElementById("min-scare").value;
+    const ghostType = document.getElementById("ghost-type").value;;
+
+    const wifiOnly = filterForm.querySelector('input[type="checkbox"]').checked;
+
+    document.getElementById("scare-label").innerText = getScareLevelText(minScare);
+
+    const filtered = allHouses.filter(house => {
+        const priceMatch = house.pricePerNight <= maxPrice;
+        const scareMatch = house.scareLevel >= minScare;
+        const ghostMatch = ghostType === "all" || house.ghostTypes.includes(ghostType);
+        const wifiMatch = wifiOnly ? house.hasWifi : true;
+
+        return priceMatch && scareMatch && ghostMatch && wifiMatch;
+    });
+
+    renderHouses(filtered);
+});
+
+fetchHouses();
